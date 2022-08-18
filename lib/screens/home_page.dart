b43d31fb3278e985554f35e39a05e7db13b0d5a1
najmulmyself <najmul.myself@gmail.com>
2022-08-18 22:43:48 +0600

    added search and record

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -67,43 +67,61 @@ class HomePage extends StatelessWidget {
                 ],
               ),
               SizedBox(
-                height: 20,
+                height: 50,
               ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.white,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    icon: Icon(
-                      Icons.search,
-                      color: Color(0xffD6D6D6),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 270,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.white,
                     ),
-                    // filled: true,
-                    // fillColor: Colors.white,
-                    hintText: 'Search for colleges, schools.....',
-                    hintStyle: TextStyle(
-                      color: Color(0xffD6D6D6),
-                      fontSize: 15,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.search,
+                          color: Color(0xffD6D6D6),
+                        ),
+                        // filled: true,
+                        // fillColor: Colors.white,
+                        hintText: 'Search for colleges, schools.....',
+                        hintStyle: TextStyle(
+                          color: Color(0xffD6D6D6),
+                          fontSize: 15,
+                        ),
+                        border: InputBorder.none,
+                        // enabledBorder: OutlineInputBorder(
+                        //   borderSide: BorderSide(
+                        //     color: Colors.white,
+                        //   ),
+                        //   borderRadius: BorderRadius.circular(18),
+                        // ),
+                        // focusedBorder: OutlineInputBorder(
+                        //   borderSide: BorderSide(
+                        //     color: Colors.white,
+                        //   ),
+                        //   borderRadius: BorderRadius.circular(30),
+                        // ),
+                        // focusedBorder: OutlineInputBorder(border),
+                      ),
                     ),
-                    border: InputBorder.none,
-                    // enabledBorder: OutlineInputBorder(
-                    //   borderSide: BorderSide(
-                    //     color: Colors.white,
-                    //   ),
-                    //   borderRadius: BorderRadius.circular(18),
-                    // ),
-                    // focusedBorder: OutlineInputBorder(
-                    //   borderSide: BorderSide(
-                    //     color: Colors.white,
-                    //   ),
-                    //   borderRadius: BorderRadius.circular(30),
-                    // ),
-                    // focusedBorder: OutlineInputBorder(border),
                   ),
-                ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Color(0xffffffff),
+                    ),
+                    child: Icon(
+                      Icons.mic,
+                      color: Color(0xff0E3C6E),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
