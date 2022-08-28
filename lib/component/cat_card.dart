d4a33e2a3a3738f13d2cd4a0cdd0b70d64a217bc
najmulmyself <najmul.myself@gmail.com>
2022-08-28 18:49:18 +0600

    added tip ,need to responsive

--- lib/component/cat_card.dart
+++ lib/component/cat_card.dart
@@ -1,17 +1,57 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class CategoryCard extends StatelessWidget {
-  String ?image;
+  String? image;
   CategoryCard({this.image});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 158, // as figma says
-      width: double.infinity,
-      child: Image.asset(
-        image!,
-        fit: BoxFit.contain,
-      ),
+    return Stack(
+      children: [
+        Container(
+          height: 158, // as figma says
+          width: double.infinity,
+          child: Image.asset(
+            image!,
+            fit: BoxFit.contain,
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                'Top Colleges',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+            // SizedBox(
+            //   height: 10,
+            // ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                'Search through thousands of accredited colleges and universities online to find the right one for you.  Apply in 3 min, and connect with your future.',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomRight,
+              child: Text(
+                '126 Colleges',
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
