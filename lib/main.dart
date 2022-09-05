cd3955f7df551ae0b2732de8f7fff16f795ef56d
najmulmyself <najmul.myself@gmail.com>
2022-09-05 14:59:18 +0600

    added nav styling

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:higher_study/screens/ano.dart';
-import 'package:higher_study/screens/container.dart';
 import 'package:higher_study/screens/home_page.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
@@ -23,68 +21,39 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       home: SafeArea(
         child: Scaffold(
-          // body: HomePage(),
-          // body: PersistentTabView(
-          //   context,
-          //   screens: [HomePage(), Container()],
-          //   items: [
-          //     PersistentBottomNavBarItem(
-          //       onPressed: (i) {},
-          //       icon: Icon(CupertinoIcons.home),
-          //       title: "Home",
-          //       activeColorPrimary: CupertinoColors.activeBlue,
-          //       inactiveColorPrimary: CupertinoColors.systemGrey,
-          //     ),
-          //     PersistentBottomNavBarItem(
-          //       icon: Icon(CupertinoIcons.settings),
-          //       title: "Settings",
-          //       activeColorPrimary: CupertinoColors.activeBlue,
-          //       inactiveColorPrimary: CupertinoColors.systemGrey,
-          //     ),
-          //     PersistentBottomNavBarItem(
-          //       icon: Icon(CupertinoIcons.settings),
-          //       title: "Settings",
-          //       activeColorPrimary: CupertinoColors.activeBlue,
-          //       inactiveColorPrimary: CupertinoColors.systemGrey,
-          //     ),
-          //     PersistentBottomNavBarItem(
-          //       icon: Icon(CupertinoIcons.settings),
-          //       title: "Settings",
-          //       activeColorPrimary: CupertinoColors.activeBlue,
-          //       inactiveColorPrimary: CupertinoColors.systemGrey,
-          //     ),
-          //     PersistentBottomNavBarItem(
-          //       icon: Icon(CupertinoIcons.settings),
-          //       title: "Settings",
-          //       activeColorPrimary: CupertinoColors.activeBlue,
-          //       inactiveColorPrimary: CupertinoColors.systemGrey,
-          //     ),
-          //   ],
-          // ),
+          // body: HomePage(), // NO NEED TO ADD HOMEPAGE | SCREEN WILL DO IT AUTOMATICALLY IN ORDER
           bottomNavigationBar: PersistentTabView(
             context,
+            backgroundColor: Color(0xff0E3C6E),
             screens: [
               HomePage(),
-              Con(),
-              ano(),
+              Container(),
+              Container(),
+              Container(),
             ],
             items: [
               PersistentBottomNavBarItem(
                 icon: Icon(CupertinoIcons.home),
                 title: "Home",
-                activeColorPrimary: CupertinoColors.activeBlue,
+                activeColorPrimary: CupertinoColors.white,
                 inactiveColorPrimary: CupertinoColors.systemGrey,
               ),
               PersistentBottomNavBarItem(
-                icon: Icon(CupertinoIcons.settings),
-                title: "Settings",
-                activeColorPrimary: CupertinoColors.activeBlue,
+                icon: Icon(Icons.bookmark_added_rounded),
+                title: "Saved",
+                activeColorPrimary: CupertinoColors.white,
                 inactiveColorPrimary: CupertinoColors.systemGrey,
               ),
               PersistentBottomNavBarItem(
-                icon: Icon(CupertinoIcons.settings),
+                icon: Icon(Icons.manage_history_sharp),
                 title: "Settings",
-                activeColorPrimary: CupertinoColors.activeBlue,
+                activeColorPrimary: CupertinoColors.white,
+                inactiveColorPrimary: CupertinoColors.systemGrey,
+              ),
+              PersistentBottomNavBarItem(
+                icon: Icon(Icons.person_sharp),
+                title: "Account",
+                activeColorPrimary: CupertinoColors.white,
                 inactiveColorPrimary: CupertinoColors.systemGrey,
               ),
             ],
