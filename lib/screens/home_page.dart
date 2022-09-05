f789d610f0225dd810b3d3894eb3905da2a87610
najmulmyself <najmul.myself@gmail.com>
2022-09-05 11:41:30 +0600

    added comment for expanded

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -14,13 +14,16 @@ class HomePage extends StatelessWidget {
         children: [
           Header(),
           Expanded(
+            // EXPANDED SHOULD BE PARENT OF LISTVIEW (NOT SURE)
             child: Padding(
               padding: EdgeInsets.only(right: 30.0, left: 30, top: 30.0),
 
               // child: CategoryCard(
               //   image: 'assets/images/cat-card1.png',
               // ),
+              // EXPANDED SHOULD NOT USED HERE BECASE ABOVE WRAPPED WITH PADDING(NOT SURE)
               child: ListView(
+                // IF WE USE COLUMN HERE ERROR OCCURED
                 children: [
                   CategoryCard(
                     image: 'assets/images/cat-card1.png',
