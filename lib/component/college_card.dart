a46066d8d2322b4b1468d384a1d3cf888853b230
najmulmyself <najmul.myself@gmail.com>
2022-09-11 01:53:53 +0600

    added college card

--- lib/component/college_card.dart
+++ lib/component/college_card.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -8,32 +8,114 @@ class CollegeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 250,
+      height: 270,
       decoration: BoxDecoration(
-        // color: Colors.red,
         borderRadius: BorderRadius.circular(30),
       ),
       child: Column(
         children: [
           Image.asset('assets/images/coll-card1.png'),
+          SizedBox(
+            height: 10,
+          ),
           Column(
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('GHJK Engineering College'),
+                  Text(
+                    'GHJK Engineering College',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
                   Container(
-                    height: 30,
+                    height: 40,
+                    width: 72,
                     decoration: BoxDecoration(
-                      color: Colors.greenAccent,
+                      color: Colors.green.shade400,
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    child: Row(
-                      children: [
-                        Text('4.3'),
-                        Icon(Icons.star),
-                      ],
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '4.3',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
                     ),
                   )
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu ut imperdiet sed nec ullamcorper.',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Color(0xff0E3C6E),
+                          textStyle: TextStyle(color: Colors.white)),
+                      onPressed: () {},
+                      child: Container(
+                        height: 20,
+                        decoration: BoxDecoration(),
+                        child: Text('Apply Now'),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Divider(
+            thickness: 2,
+            color: Colors.grey.shade400,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(
+                Icons.thumbs_up_down_sharp,
+                color: Color(0xff0E3C6E),
+              ),
+              Text(
+                'More than 1000+ students have been..',
+                style: TextStyle(color: Colors.grey),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.remove_red_eye,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    '468+',
+                    style: TextStyle(color: Colors.grey),
+                  ),
                 ],
               )
             ],
